39a65e1af9789b2582f9fe9cfa8d00f5aaf7e25e
Peter <themingwe@gmail.com>
2023-02-25 17:47:15 +0200

    added github pages and changed fetch url to https

--- src/redux/squaresAPI.js
+++ src/redux/squaresAPI.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export function fetchPresets() {
-  return axios.get('http://demo7919674.mockable.io/')
+  // return axios.get('http://demo7919674.mockable.io/')
+  return axios.get('https://demo7919674.mockable.io/')
 }
